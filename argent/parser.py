66afa0a308817df82aa9499d2985b158ddee6473
tim <tdixon51793@gmail.com>
2012-01-05 00:56:16 -0600

    Fleshed out parser.parse a little more.
    Now, it will check if there are any subcommands in the list of
    arguments. If there is one, run that subpartser with all the arguments
    after the subcommand; otherwise, run the Parser's function with the
    arguments.

--- argent/parser.py
+++ argent/parser.py
@@ -11,7 +11,7 @@ def nothing(*args, **kwargs):
 class Parser(object):
     """A parser for command-line flags and arguments."""
     def __init__(self, function=nothing):
-        # a dictionary of subparsers; the keys are their names, the values are the actual objects:
+        # a dictionary of subparsers; the keys are their names, the values are the actual objects.
         self.subparsers = defaultdict(Parser)
         # this is the function that will get all of the arguments passed to the parser.
         self.function = function
@@ -20,6 +20,27 @@ class Parser(object):
         """A decorator that creates a new subparser in self.subparsers from the function."""
         self.subparsers[fn.__name__].function = fn
     
-    def parse(self, args):
-        """Given some command-line arguments, do stuff with them."""
-        pass
+    def first_subcommand(self, arguments):
+        """Given a list of arguments, return the first argument that corresponds to a subcommand
+        and its index in the list. If none of them do, return None.
+        """
+        for n, a in enumerate(arguments):
+            if a in self.subparsers.keys():
+                return a, n
+        return None
+
+    def parse(self, arguments):
+        """Given some command-line arguments, decide what to do with them."""
+        # get the first subcommand of the arguments, or None if there aren't any.
+        subcommand = self.first_subcommand(arguments)
+        # if there is a subcommand:
+        if subcommand:
+            (name, index) = subcommand
+            # pass all of the subcommands after the subcommand name
+            args_to_pass = arguments[index + 1:]
+            # give them to the subparser, to parse
+            self.subparsers[name].parse(args_to_pass)
+        # otherwise, run self.function with the arguments
+        else:
+            self.function(*arguments)
+
