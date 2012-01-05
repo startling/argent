5f7cc3d028341a2fcbe6bccba74901ef34fc5c2f
tim <tdixon51793@gmail.com>
2012-01-04 23:43:52 -0600

    Basic setup.py, nothing special yet.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import argent
+
+setup(
+    name = "argent",
+    version = argent.__version__,
+    author = "startling",
+    author_email = "tdixon51793@gmail.com",
+    description = "A module for parsing command-line arguments and flags; simpler than argparse.",
+    packages = ["argent"],
+    install_requires = [],
+)
