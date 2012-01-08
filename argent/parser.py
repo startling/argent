103ef9bba04fdb12d8d07c114d8567041f52a0a3
tim <tdixon51793@gmail.com>
2012-01-08 01:58:42 -0600

    made things PEP8 compliant and changed some documentation and comments.
    wooh PEP8!

--- argent/parser.py
+++ argent/parser.py
@@ -3,18 +3,22 @@
 from sys import argv
 import inspect
 
+
 def nothing(*args, **kwargs):
-    """A function that does nothing. This is used because it's nice for `Parser` objects
-    to always have a function as their obj.function attribute. 
+    """A function that does nothing. This is used because it's nice for
+    Parser objects to always have a function as their obj.function attribute.
     """
     pass
+
 
 class Parser(object):
     """A parser for command-line flags and arguments."""
     def __init__(self, function=nothing, flags=[]):
-        # a dictionary of subparsers; the keys are their names, the values are the actual objects.
+        # a dictionary of subparsers;
+        # the keys are their names, the values are the actual objects.
         self.subparsers = {}
-        # this is the function that will get all of the arguments passed to the parser.
+        # this is the function that will get all of the arguments passed
+        # to the parser.
         self.function = function
         # and these are the flags that it can take
         self.flags = flags
@@ -33,19 +37,22 @@ class Parser(object):
         parser.args = [a for a in args if not a.startswith("_")]
         # return it.
         return parser
-    
+
     def subparse(self, fn):
-        """A decorator that creates a new subparser in self.subparsers from the function."""
+        """A decorator that creates a new subparser in self.subparsers
+        from the decorated function.
+        """
         # create a parser from this function...
         subparser = Parser.from_function(fn)
         # add it to the `subparsers` dictionary.
         self.subparsers[subparser.name] = subparser
         # return the Parser object for the subparser.
         return subparser
-    
+
     def first_subcommand(self, arguments):
-        """Given a list of arguments, return the first argument that corresponds to a subcommand
-        and its index in the list. If none of them do, return None.
+        """Given a list of arguments, return the first argument that
+        corresponds to a subcommand and its index in the list. If none
+        of them do, return None.
         """
         for n, a in enumerate(arguments):
             if a in self.subparsers.keys():
@@ -54,7 +61,7 @@ class Parser(object):
 
     def parse(self, arguments):
         """Given some command-line arguments, decide what to do with them."""
-        # get the first subcommand of the arguments, or None if there aren't any.
+        # get the first subcommand of the arguments, or None.
         subcommand = self.first_subcommand(arguments)
         # if there is a subcommand:
         if subcommand:
@@ -65,14 +72,16 @@ class Parser(object):
             self.subparsers[name].parse(args_to_pass)
         # otherwise, run self.function with the arguments
         else:
-            # get flags from arguments -- flags are anything that starts with `-`.
-            # (instead of the flag's actual name, use its name
-            # with underscores rather than dashes)
-            flags = [a.replace("-", "_") for a in arguments if 
+            # get flags from arguments -- flags are anything that starts with
+            # a dash ("-"). Instead of the flag's actual name, use its name
+            # with underscores rather than dashes.
+            flags = [a.replace("-", "_") for a in arguments if
                     a.startswith("-")]
             # strip the flags from the arguments list:
             arguments = [a for a in arguments if not a.startswith("-")]
-            # check that there aren't any flags here that aren't defined in self.flags:
+            # check that there aren't any flags here that aren't
+            # defined in self.flags; i.e., check that the given flags
+            # is a subset of the possible flags.
             if not set(self.flags) >= set(flags):
                 raise NameError("Illegal arguments.")
             # create a dictionary of used flags here, to be passed as kwargs.
@@ -85,4 +94,3 @@ class Parser(object):
         # since the first argument is the name of the file, ignore it.
         arguments = argv[1:]
         self.parse(arguments)
-
