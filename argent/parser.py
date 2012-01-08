8b9ffaefe83c153e83d2909c0d968967a6f9e32e
tim <tdixon51793@gmail.com>
2012-01-08 01:46:08 -0600

    Deduce flags and poisitonal arguments from function arguments.
    There's a lot of code here, so I'll explain.
    * the classmethod `from_function` gets the function's expected arguments
        using `inspect.getargspec(fn)`. If they start with an underscore or
        two, they're flags; otherwise, positional arguments.
    * the subparse decorator is a lot simpler now, because it doesn't
        figure out flags itself.
    * changed `parse` a little to use new underscored flags.

--- argent/parser.py
+++ argent/parser.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from sys import argv
+import inspect
 
 def nothing(*args, **kwargs):
     """A function that does nothing. This is used because it's nice for `Parser` objects
@@ -24,21 +25,23 @@ class Parser(object):
         parser = cls(fn)
         # figure out the parser's name.
         parser.name = fn.__name__
+        # get the arguments the function expects
+        args, _, _, _ = inspect.getargspec(fn)
+        # flags are the ones that start with an underscore:
+        parser.flags = [a for a in args if a.startswith("_")]
+        # arguments are the ones that don't.
+        parser.args = [a for a in args if not a.startswith("_")]
         # return it.
         return parser
     
-    def subparse(self, flags=[]):
+    def subparse(self, fn):
         """A decorator that creates a new subparser in self.subparsers from the function."""
-        def add_subparser(fn):
-            # create a parser from this function...
-            subparser = Parser.from_function(fn)
-            # give it the flags it should have.
-            subparser.flags = flags
-            # add it to the `subparsers` dictionary.
-            self.subparsers[subparser.name] = subparser
-            # return the Parser object for the subparser.
-            return subparser
-        return add_subparser
+        # create a parser from this function...
+        subparser = Parser.from_function(fn)
+        # add it to the `subparsers` dictionary.
+        self.subparsers[subparser.name] = subparser
+        # return the Parser object for the subparser.
+        return subparser
     
     def first_subcommand(self, arguments):
         """Given a list of arguments, return the first argument that corresponds to a subcommand
@@ -63,15 +66,17 @@ class Parser(object):
         # otherwise, run self.function with the arguments
         else:
             # get flags from arguments -- flags are anything that starts with `-`.
-            flags = [a for a in arguments if a.startswith("-")]
+            # (instead of the flag's actual name, use its name
+            # with underscores rather than dashes)
+            flags = [a.replace("-", "_") for a in arguments if 
+                    a.startswith("-")]
             # strip the flags from the arguments list:
             arguments = [a for a in arguments if not a.startswith("-")]
             # check that there aren't any flags here that aren't defined in self.flags:
             if not set(self.flags) >= set(flags):
                 raise NameError("Illegal arguments.")
             # create a dictionary of used flags here, to be passed as kwargs.
-            # (instead of the flag's actual name, use instead its name without dashes)
-            flag_dict = dict(((f.replace("-", ""), True) for f in flags))
+            flag_dict = dict(((f, True) for f in flags))
             # call the function with the arguments and flags.
             self.function(*arguments, **flag_dict)
 
