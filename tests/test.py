9e8734da386c98efe4ad1ae0daa97fb0b491d6af
tim <tdixon51793@gmail.com>
2012-01-10 16:05:41 -0600

    Tests for when argent guesses necessary and optional args and flags.
    Should've had these here a while ago. Hm. Oh well.

--- tests/test.py
+++ tests/test.py
@@ -39,6 +39,24 @@ class FirstCase(unittest.TestCase):
     def test_positional_arguments(self):
         "Test that the hello subcommand works when you give it an argument."
         self.assertParser(["hello", "--f", "vuiqqwibuqw"], "vuiqqwibuqw")
+    
+    def test_guess_optional_arguments(self):
+        "Test that argent guesses the optional arguments correctly."
+        hello_parser = self.parser.subparsers["hello"]
+        assert "something" in [a.name for a in hello_parser.optional_args]
+
+    def test_guess_necessary_arguments(self):
+        "Test that argent guesses the necessary arguments correctly."
+        something_parser = self.parser.subparsers["something"]
+        necessary_args = [a.name for a in something_parser.necessary_args]
+        assert "a" in necessary_args
+        assert "b" in necessary_args
+
+    def test_guess_flags(self):
+        "Test that argent guesses which args are flags correctly."
+        hello_parser = self.parser.subparsers["hello"]
+        assert "--f" in [f.name for f in hello_parser.flags]
+
 
 if __name__ == "__main__":
     unittest.main()
